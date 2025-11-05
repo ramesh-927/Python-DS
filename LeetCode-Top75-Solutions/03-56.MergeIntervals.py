8aa974e3bcd2f3fe5693441f00dbfd6b254a1cdc
rameshbabugolla <61953033+rameshbabugolla@users.noreply.github.com>
2025-11-04 18:54:48 -0600

    Leetcode Top 75 Solution   added

---
+++ LeetCode-Top75-Solutions/03-56.MergeIntervals.py
@@ -0,0 +1,55 @@
+"""
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+Example 3:
+Input: intervals = [[4,7],[1,4]]
+Output: [[1,7]]
+Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+Constraints:
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+"""
+class Solution:
+    def mergeIntervals(self, intervals):
+        if not intervals:
+            return []
+        merged = [intervals[0]]
+        intervals.sort(key = lambda x : x[0])
+
+        for start, end in intervals[1:]:
+            last_end = merged[-1][1]
+
+            if start < last_end:
+                merged[-1][1] = max(last_end, end)
+            else:
+                merged.append([start, end])
+        return merged
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    tests = [
+        ([[1,3],[2,6],[8,10],[15,18]], [[1,6],[8,10],[15,18]]),
+        ([[1,4],[4,5]], [[1,5]]),
+        ([], []),
+        ([[1,4],[0,2],[3,5]], [[0,5]]),
+    ]
+    for input, expected in tests:
+        output = sol.mergeIntervals(input)
+        print(f"{input} -> {output})")
+
+
+
+#  Time     | **O(n log n)**         
+#  Space    | **O(n)** in worst case 
+
+
