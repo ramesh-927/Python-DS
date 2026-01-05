745a58dacebad7ca1b9b7fc4247d2bead268f7d2
rameshbabugolla <61953033+rameshbabugolla@users.noreply.github.com>
2026-01-05 12:43:36 -0600

    etract error last 1 hour file  added

--- Log-ParsingInterview-Questions-SRE/05-extractErrorslast1hour.py
+++ Log-ParsingInterview-Questions-SRE/05-extractErrorslast1hour.py
@@ -4,20 +4,22 @@ Q3. Suppose logs contain timestamps. How would you extract only the errors in th
 """
 import re
 from datetime import datetime, timedelta
+class Solution:
+    def recent_errors(self, file_path):
+        time_pattern = re.compile(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}")  # e.g. 2025-08-30 10:15:32
+        hour_ago = datetime.now() - timedelta(hours=1)
 
-def recent_errors(file_path):
-    time_pattern = re.compile(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}")  # e.g. 2025-08-30 10:15:32
-    hour_ago = datetime.now() - timedelta(hours=1)
-
-    with open(file_path, "r") as f:
-        for line in f:
-            match = time_pattern.search(line)
-            if match and "ERROR" in line:
-                timestamp = datetime.strptime(match.group(), "%Y-%m-%d %H:%M:%S")
-                if timestamp >= hour_ago:
-                    yield line.strip()
+        with open(file_path, "r") as f:
+            for line in f:
+                match = time_pattern.search(line)
+                if match and "ERROR" in line:
+                    timestamp = datetime.strptime(match.group(), "%Y-%m-%d %H:%M:%S")
+                    if timestamp >= hour_ago:
+                        yield line.strip()
 
 # Example
-for error in recent_errors("/Users/rameshgolla/Python-DS/Python-DS/log-fileparsing/error-code.log"):
-    print(error)
+if __name__== "__main__":
+    sol = Solution()
+    for error in sol.recent_errors("error-code.log"):
+        print(error)
 
